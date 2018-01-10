324a7ea457a6ccbbb65eb0c3b1f5d201a7271647
redhawkIT <khodkodmitry10011983@hotmail.com>
2018-01-09 18:09:44 -0800

    Abstracted out storage.

---
+++ app/containers/App.js
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import MainSection from '../components/MainSection';
+// import * as TodoActions from '../actions/todos';
+import style from './App.css';
+
+import { addTodo, deleteTodo, editTodo, completeTodo, completeAll, clearCompleted } from '../ducks/todos'
+const TodoActions = { addTodo, deleteTodo, editTodo, completeTodo, completeAll, clearCompleted }
+
+@connect(
+  state => ({
+    todos: state.todos
+  }),
+  dispatch => ({
+    actions: bindActionCreators(TodoActions, dispatch)
+  })
+)
+export default class App extends Component {
+
+  static propTypes = {
+    todos: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+  };
+
+  render() {
+    const { todos, actions } = this.props;
+
+    return (
+      <div className={style.normal}>
+        <Header addTodo={actions.addTodo} />
+        <MainSection todos={todos} actions={actions} />
+      </div>
+    );
+  }
+}
